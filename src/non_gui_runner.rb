e1a8b7bf2cfcf90086bd27a26d673b1e1f98ba4c
Matt Morris <mattmorris@matt-morriss-imac.local>
2009-08-10 08:29:44 -0600

    more cleanup

--- src/non_gui_runner.rb
+++ src/non_gui_runner.rb
@@ -6,15 +6,14 @@ class NonGuiRunner
 
   def initialize
     @logger_info =
-    [
-            #:tick_count,
-            :ship_position,
-            #:ship_velocity,
-            #:ship_bank,
-            :star_eaten,
-            :star_reward,
-            #:star_info_hash,
-            :action_info
+    [ #:tick_count,
+      :ship_position,
+      #:ship_velocity,
+      #:ship_bank,
+      :star_eaten,
+      :star_reward,
+      #:star_info_hash,
+      :action_info
     ]
     $LOGGER = Logging.logger(STDOUT, :pattern => "%m\n", :date_pattern => "")
     $LOGGER.level = :debug
@@ -22,22 +21,28 @@ class NonGuiRunner
 
   def run_game
 
-    brains = [:reinforcement]
+    brain_types = [:reinforcement]
+    star_collection_types = [:progressive]
 
-    brains.each do |brain_type|
+    brain_types.each do |brain_type|
 
-      @game = Game.new(:size_x => 800, :size_y => 600, :num_stars => 3, :brain_type => brain_type)
-      @tick_count = 0
+      star_collection_types.each do |star_collection_type|
 
-      200.times do
-        @tick_count += 1
-        @game.tick
-        show_log_info
+        @game = Game.new(:size_x => 800, :size_y => 600, :num_stars => 3, :brain_type => brain_type, :star_collection_type => star_collection_type)
+        @tick_count = 0
+
+        100.times do
+          @tick_count += 1
+          @game.tick
+          show_log_info
+        end
+
+        puts "*" * 50
+        puts "Brain type #{brain_type.to_s} accumulated #{@game.environment.bank} points when running with star collection type #{star_collection_type}"
+        puts "*" * 50
+
       end
-      
-      puts "*" * 50
-      puts "Brain type #{brain_type.to_s} accumulated #{@game.environment.bank} points"
-      puts "*" * 50
+        
     end 
   end
 
@@ -53,7 +58,6 @@ class NonGuiRunner
     $LOGGER.info("ship bank = #{$GAME_INFO[:ship_bank]}") if @logger_info.include?(:ship_bank) and $GAME_INFO[:ship_bank]
     $LOGGER.info("star eaten id = #{$GAME_INFO[:star_eaten]}") if @logger_info.include?(:star_eaten) and $GAME_INFO[:star_eaten]
     $LOGGER.info("star reward = #{$GAME_INFO[:star_reward]}") if @logger_info.include?(:star_reward) and $GAME_INFO[:star_reward]
-
 
     # reset all GAME INFO to nil so old info doesn't stick around for next tick
     $GAME_INFO.each_key {|k| $GAME_INFO[k] = nil}
