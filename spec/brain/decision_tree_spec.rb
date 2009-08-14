3c9686981ed77a4d030392c52b353f71194e73cc
Ian Schreuder <ian.schreuder@gmail.com>
2009-08-14 07:02:48 -0700

    merge fix

--- spec/brain/decision_tree_spec.rb
+++ spec/brain/decision_tree_spec.rb
@@ -11,7 +11,6 @@ describe DecisionTreeBrain do
     setup
   end
 
-<<<<<<< HEAD:spec/brain/decision_tree_spec.rb
   it "should return a next velocity" do
     @brain.set_data(@data, 5)
     @brain.next_velocity.class.should == Velocity
@@ -21,30 +20,13 @@ describe DecisionTreeBrain do
     @brain.set_data(@data, 5)
     
   end
-=======
-  it "should return a next velocity" #do
-  #  star = mock(Star)
-  #  star.should_receive()
-  #  @environment.should_receive(:data).and_return({:stars => })
-  #  @brain.should_receive(:next_velocity).and_return(Velocity.new_with_xy(0,0))
-  #  @brain.ship.tick(mock(Environment)).class.should be(Velocity)
-  #end
-
-  it "should add a captured star to the training data set with correct attributes"
->>>>>>> fffd8f65f94df4f3aad1ec0f694f3eb9052b13f0:spec/brain/decision_tree_spec.rb
 
   private
 
   def setup
     @brain = DecisionTreeBrain.new
-<<<<<<< HEAD:spec/brain/decision_tree_spec.rb
     @data = {:star_position_hash => {1 => Position.new(25,25), 2 => Position.new(100,0)},
              :ship_position => Position.new(0,0), :reward => nil, :star_id_delivering_reward => nil}
-=======
-    position = Position.new(0,0)
-    ship = Ship.new(@brain, position)
-    @environment = mock(Environment)
->>>>>>> fffd8f65f94df4f3aad1ec0f694f3eb9052b13f0:spec/brain/decision_tree_spec.rb
   end
 
 end
--- src/brains/decision_tree_brain.rb
+++ src/brains/decision_tree_brain.rb
@@ -2,7 +2,6 @@ require "ai4r"
 
 class DecisionTreeBrain
   def initialize
-<<<<<<< HEAD:src/brains/decision_tree_brain.rb
     @data_labels = ["Value Group", "Worthy Target"]
     @data_items = []
     @target = {:id => nil, :position => nil}
@@ -65,47 +64,5 @@ class DecisionTreeBrain
     @data[:star_position_hash].each{|id, pos| target = {:id => id, :position => pos} if target.nil? || @ship_position.distance(pos) < @ship_position.distance(target[:position])}
     target || {:id => nil, :position => @ship_position}
   end
-=======
-    @data_labels = ["Ticks", "Color", "Reward", "Value Group"]
-    @data_items = []
-  end
-
-  def next_velocity(ship)
-    return get_decition_tree_next_velocity(ship) if @data_items.length > 20
-
-    verify_target
-
-    Velocity.new(ship.position.get_vector_to(@target))
-
-  end
-
-  private
-
-  def verify_target
-    if @target.nil? || !star_at_position(@target)
-      @target = @ship.star_position_hash[rand(@ship.star_position_hash.length)]
-      
-    end
-  end
-  
-  def star_at_position(position)
-    @ship.star_position_hash.detect{|key,value| value == position }
-  end
-
-  def get_decision_tree_next_velocity(ship)
-    
-  end  
-  
-  def analyze
-    data_set = DataSet.new(:data_items=>@data_items, :data_labels=>@data_labels)
-    id3 = Ai4r::Classifiers::ID3.new.build(data_set)
-    id3.get_rules
-    id3.eval(['New York', '<30', 'M'])
-  end
-
-  def add_star_to_data_items(star)
-    
-  end  
->>>>>>> fffd8f65f94df4f3aad1ec0f694f3eb9052b13f0:src/brains/decision_tree_brain.rb
 
 end
