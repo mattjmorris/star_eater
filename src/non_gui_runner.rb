4be925dfc213c2a3190c10ca427ba27cd7323091
Ian Schreuder <ian@Josephine.local>
2009-08-16 14:02:02 -0600

    merged decision tree

--- src/non_gui_runner.rb
+++ src/non_gui_runner.rb
@@ -22,7 +22,7 @@ class NonGuiRunner
   def run_game
 
     #brain_types = [:decisiontree, :reinforcement]
-    brain_types = [:decisiontree]
+    brain_types = [:decisiontree, :reinforcement]
     star_collection_types = [:progressive]
 
     brain_types.each do |brain_type|
